be33d18edf5adfd1de9a909cad23372472a290c5
priyagupta314-work <75786939+priyagupta314-work@users.noreply.github.com>
2022-12-28 17:21:19 +0530

    check bill amount < cash paid

--- index.js
+++ index.js
@@ -6,12 +6,6 @@
             const changeAmountEl = document.querySelector("#change-amount");
             console.log({changeAmountEl})
             const notesCountEl = document.querySelectorAll(".notesCount");
-            
-            for (let i=0;i<notesCountEl.length;i++){
-                console.log("in if loop")
-                console.log(notesCountEl)
-            }
-            //console.log({notesCountEl});
             const denominationsEl = document.querySelectorAll(".denominations");
             console.log({denominationsEl})
             
@@ -19,18 +13,23 @@
             function calculateAmount(){
                 const userBillAmount = billAmountEl.value;
                 const cashPaidByUser = cashPaidEl.value;
+                const denominations = [2000, 500, 100, 50, 20, 10, 5, 1];
                 let changeAmount = parseInt(cashPaidByUser) - parseInt(userBillAmount);
-                changeAmountEl.innerText = `Please return Rs. ${changeAmount} to the customer`
-                const denominations = [2000, 500, 100, 20, 10, 5, 1];
-                
-                for(let i=0;i<denominations.length;i++){
+                if (changeAmount < 0){
+                    changeAmountEl.innerText = `Insufficient money!!`
+                }
+
+                else{
+                    changeAmountEl.innerText = `Please return Rs. ${changeAmount} to the customer`
+                               
+                     for(let i=0;i<denominations.length;i++){
                     // denominationsEl.innerText[i] = denominations[i];
-                    const numberOfNotes = Math.trunc(changeAmount/denominations[i]);
-                    console.log({numberOfNotes})
-                    console.log({notesCountEl})
-                    notesCountEl[i].innerText = numberOfNotes;
-                    changeAmount = changeAmount % denominations[i];
+                        const numberOfNotes = Math.trunc(changeAmount/denominations[i]);
+                        notesCountEl[i].innerText = numberOfNotes;
+                        changeAmount = changeAmount % denominations[i];
                   
+                }
+                
                 }
                 //console.log({numberOfNotes});
                 
