652ed86f21a01465e43eb23f9d39bd5558726ea2
priyagupta314-work <75786939+priyagupta314-work@users.noreply.github.com>
2022-12-28 16:28:16 +0530

    initial commit for all files

---
+++ index.js
@@ -0,0 +1,41 @@
+
+            
+            const billAmountEl = document.querySelector("#bill-amount");
+            const cashPaidEl = document.querySelector("#cash-paid");
+            const submitEl= document.querySelector("#submit");
+            const changeAmountEl = document.querySelector("#change-amount");
+            console.log({changeAmountEl})
+            const notesCountEl = document.querySelectorAll(".notesCount");
+            
+            for (let i=0;i<notesCountEl.length;i++){
+                console.log("in if loop")
+                console.log(notesCountEl)
+            }
+            //console.log({notesCountEl});
+            const denominationsEl = document.querySelectorAll(".denominations");
+            console.log({denominationsEl})
+            
+            submitEl.addEventListener("click",calculateAmount,false);
+            function calculateAmount(){
+                const userBillAmount = billAmountEl.value;
+                const cashPaidByUser = cashPaidEl.value;
+                let changeAmount = parseInt(cashPaidByUser) - parseInt(userBillAmount);
+                changeAmountEl.innerText = `Change money: ${changeAmount} INR`
+                const denominations = [2000, 500, 100, 20, 10, 5, 1];
+                
+                for(let i=0;i<denominations.length;i++){
+                    // denominationsEl.innerText[i] = denominations[i];
+                    const numberOfNotes = Math.trunc(changeAmount/denominations[i]);
+                    console.log({numberOfNotes})
+                    console.log({notesCountEl})
+                    notesCountEl[i].innerText = numberOfNotes;
+                    changeAmount = changeAmount % denominations[i];
+                  
+                }
+                //console.log({numberOfNotes});
+                
+                
+
+            }
+        
+    
